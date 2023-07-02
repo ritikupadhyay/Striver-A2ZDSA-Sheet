121f32930b7b1ddc5d58b9d9dfcb998ef71d9c6e
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2023-07-02 16:03:21 +0530

    Update Pattern1.java

--- Pattern/Pattern1.java
+++ Pattern/Pattern1.java
@@ -1,1 +1,9 @@
+    public static void pattern(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+    }

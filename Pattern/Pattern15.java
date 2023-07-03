2b3e8c6f5dc64f3eda33c590b1df995dda1c64dd
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2023-07-03 22:21:11 +0530

    Create Pattern15.java

---
+++ Pattern/Pattern15.java
@@ -0,0 +1,12 @@
+public static void pattern(int n){
+
+        for (int i=1;i<=n;i++){
+            char st='A';
+            for(int j=0;j<=n-i;j++){
+                System.out.print(st+" ");
+                st++;
+            }
+
+            System.out.println();
+        }
+    }

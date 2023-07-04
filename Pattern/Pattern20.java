d4ddade869008b27c54d37683adad031dab6800f
ritikupadhyay <97518015+ritikupadhyay@users.noreply.github.com>
2023-07-04 22:58:31 +0530

    Create Pattern20.java

---
+++ Pattern/Pattern20.java
@@ -0,0 +1,26 @@
+    public static void pattern(int n){
+        for (int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            for(int k=0;k<2*(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int l=0;l<i;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i=1;i<n;i++){
+            for(int j=0;j<n-i;j++){
+                System.out.print("*");
+            }
+            for(int k=0;k<2*(i);k++){
+                System.out.print(" ");
+            }
+            for(int l=0;l<n-i;l++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
